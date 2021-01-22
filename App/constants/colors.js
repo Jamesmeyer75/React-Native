6637812a499a2d25b3143a533a3c6188a9d8b826
James Meyer <Jamesmeyer75@gmail>
2021-01-22 17:20:28 -0600

    Added text input, styling, and current date with date fns package

--- App/constants/colors.js
+++ App/constants/colors.js
@@ -1,5 +1,8 @@
 export default {
     text: '#343434',
     border: '#E2E2E2',
-    blue: '#688bec'
+    blue: '#688bec',
+    white: '#fff',
+    textLight: '#797979',
+    offWhite: '#f0f0f0'
 }
--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,9 +1,11 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
 import React from 'react';
-import { View, StyleSheet, StatusBar, Image, Dimensions } from 'react-native';
+import { View, StyleSheet, StatusBar, Image, Dimensions, Text } from 'react-native';
+import { format } from 'date-fns';
 
 import colors from '../constants/colors';
+import { ConversionInput } from '../components/ConversionInput';
 
 const screen = Dimensions.get('window');
 
@@ -25,10 +27,27 @@ const styles = StyleSheet.create({
             position: "absolute",
             width: screen.width * 0.25,
             height: screen.width * 0.25
+        },
+        textHeader: {
+            color: colors.white,
+            fontWeight: "bold",
+            fontSize: 30,
+            marginVertical: 20,
+            textAlign: "center"
+        },
+        text: {
+            color: colors.white,
+            fontSize: 14,
+            textAlign: "center"
         }
 });
 
 export default () => {
+    const baseCurrency = "USD";
+    const quoteCurrency = "GBP";
+    const conversionRate = 0.8345;
+    const date = new Date();
+
     return (
         <View style={styles.container}>
             <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
@@ -36,6 +55,26 @@ export default () => {
                 <Image source={require("../assets/images/background.png")} style={styles.logoBackground} resizeMode="contain" />
                 <Image source={require("../assets/images/logo3.png")} style={styles.logo} resizeMode="contain" />
             </View>
+
+            <Text style={styles.textHeader}>Currency Converter</Text>
+
+            <ConversionInput 
+                text={baseCurrency}
+                value="123"
+                onButtonPress={() => alert("todo")}
+                onChangeText={text => console.log('text', text)}
+                keyboardType="numeric"
+            />
+            <ConversionInput 
+                text={quoteCurrency}
+                value="123"
+                onButtonPress={() => alert("todo")}
+                editable={false}
+            />
+
+            <Text style={styles.text}>
+                {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do, yyyy')}.`}
+            </Text>
         </View>
     );
 };
